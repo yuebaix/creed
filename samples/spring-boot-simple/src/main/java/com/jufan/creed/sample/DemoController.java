f39214eef4ee4f724cd934bcb94f57d85713ace2
JerryNiu <huifumanlove@hotmail.com>
2018-07-31 20:02:18 +0800

    添加测试各种插件

--- samples/spring-boot-simple/src/main/java/com/jufan/creed/sample/DemoController.java
+++ samples/spring-boot-simple/src/main/java/com/jufan/creed/sample/DemoController.java
@@ -4,12 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * <p>Description: 示例接口</p>
- *
- * @author Jerry
- * @date 2018-07-30 21:28
- */
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
--- samples/spring-boot-simple/src/main/java/com/jufan/creed/sample/SimpleApp.java
+++ samples/spring-boot-simple/src/main/java/com/jufan/creed/sample/SimpleApp.java
@@ -4,11 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * <p>Description: spring2.0简单例子</p>
- * @author Jerry
- * @date 2018-07-30 21:04
- */
 @Slf4j
 @SpringBootApplication
 public class SimpleApp {
