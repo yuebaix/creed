ea11d6e2f448b70b7873b55f6a09c0f23de5a2a4
JerryNiu <huifumanlove@hotmail.com>
2018-08-07 17:30:39 +0800

    调整测试checkstyle与javadoc

---
+++ samples/springboot-simple/src/main/java/com/geercode/creed/samples/NumbO.java
@@ -0,0 +1,14 @@
+package com.geercode.creed.samples;
+
+import lombok.Data;
+
+/**
+ * <p>Description: </p>
+ *
+ * @author Jerry
+ */
+@Data
+public class NumbO {
+	private long id;
+	private String name;
+}
