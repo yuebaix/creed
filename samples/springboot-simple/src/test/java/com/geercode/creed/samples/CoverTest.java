5df418b34f653f2dade2dd8ab4a5ef87d9ba138d
JerryNiu <huifumanlove@hotmail.com>
2018-08-07 12:48:56 +0800

    测试coveralls

---
+++ samples/springboot-simple/src/test/java/com/geercode/creed/samples/CoverTest.java
@@ -0,0 +1,13 @@
+package com.geercode.creed.samples;
+
+import org.junit.Test;
+
+/**
+ * @author Jerry
+ */
+public class CoverTest {
+	@Test
+	public void test1() {
+		System.out.println("coveralls");
+	}
+}
