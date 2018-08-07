f3c61313e66ee56ba86f2e8c5085c0a0b5905873
JerryNiu <huifumanlove@hotmail.com>
2018-08-07 18:33:39 +0800

    调整测试checkstyle与javadoc

--- samples/springboot-simple/src/main/java/com/geercode/creed/samples/DemoController.java
+++ samples/springboot-simple/src/main/java/com/geercode/creed/samples/DemoController.java
@@ -5,11 +5,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * <p>Title       : DemoController</p>
+ * <p>Description : Demo 接口</p>
+ * <p>Copyright   : Copyright 2018-2050</p>
+ * <p>Company     : www.juxiangfen.com</p>
+ * <p>created on  : 2018-08-07 18:30</p>
+ *
  * @author jerryniu
+ * @version 1.0.0
  */
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
+	/**
+	 * <p>description : 测试接口</p>
+	 * <p>create   on : 2018-08-07 18:31:22</p>
+	 * @author: jerryniu
+	 */
 	@GetMapping("/hello")
 	public String hello() {
 		return "hello";
--- samples/springboot-simple/src/main/java/com/geercode/creed/samples/SimpleApp.java
+++ samples/springboot-simple/src/main/java/com/geercode/creed/samples/SimpleApp.java
@@ -5,11 +5,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * <p>Title       : SimpleApp</p>
+ * <p>Description : 简单示例启动类</p>
+ * <p>Copyright   : Copyright 2018-2050</p>
+ * <p>Company     : www.juxiangfen.com</p>
+ * <p>created on  : 2018-08-07 18:32</p>
+ *
  * @author jerryniu
+ * @version 1.0.0
  */
 @Slf4j
 @SpringBootApplication
 public class SimpleApp {
+	/**
+	 * <p>description : 程序入口</p>
+	 * <p>create   on : 2018-08-07 18:32:40</p>
+	 * @author: jerryniu
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleApp.class, args);
 	}
