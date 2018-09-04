45f160a97af2ee10a1154262f39283fc17653670
JerryNiu <huifumanlove@hotmail.com>
2018-09-04 17:14:39 +0800

    修改发布脚本

--- release.bat
+++ release.bat
@@ -1,12 +1,12 @@
 @echo off
 rem #######################################################################################
-rem 发布release (kjar使用asm编译字节码的时候不支持中文，不要在变量中用中文)
+rem 发布release
 rem #######################################################################################
 
-rem set /p version=plz set a useful version...:
-rem if "%version%"=="" goto releasefailed
+set /p version=plz set a useful version...:
+if "%version%"=="" goto releasefailed
 
-set version=1.0-PROD-SNAPSHOT
+rem set version=1.0-PROD-SNAPSHOT
 
 echo ***************************************************************************************
 echo change version to %version%...
@@ -17,7 +17,7 @@ rem 确认可以编译通过
 echo ***************************************************************************************
 echo make sure compile is working
 echo ***************************************************************************************
-call mvn clean install -DskipTests
+call mvn clean install -Pjufan
 if "%errorlevel%"=="1" goto releasefailed
 goto releasesuccess
 
@@ -26,7 +26,7 @@ rem 检查工程依赖树
 echo ***************************************************************************************
 echo inspect dependency tree
 echo ***************************************************************************************
-call mvn dependency:tree -Dverboss -Dincludes=com.jufan.cloud.rules:rules
+call mvn dependency:tree -Pjufan -Dverboss
 
 set /p input=deploy version is %version%,continue?(y/n):
 if "%input%"=="n" goto releasefailed
@@ -36,7 +36,7 @@ rem 发布动作
 echo ***************************************************************************************
 echo deploy release...
 echo ***************************************************************************************
-call mvn clean deploy -DskipTests
+call mvn clean deploy -Pjufan -DskipTests
 if "%errorlevel%"=="1" goto releasefailed
 
 call mvn versions:revert
