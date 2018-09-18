b5aef1cd32811e2bf5cbaa534a46e44484362ae6
JerryNiu <huifumanlove@hotmail.com>
2018-09-18 20:16:47 +0800

    添加web层baseResp与baseCode

---
+++ samples/springboot-multi/springboot-multi-web/src/main/java/com/geercode/creed/samples/web/common/BaseCode.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2018-2050 the original author or authors.
+ * Powered by Yimei Corp.
+ * All Rights Reserved.
+ */
+
+package com.geercode.creed.samples.web.common;
+
+/**
+ * <p>description : 基础回复编码</p>
+ *
+ * @author jerryniu
+ * @version 1.0.0
+ */
+public enum BaseCode {
+    /**成功*/
+    SUCCESS("1000", "成功"),
+    /**失败*/
+    FAIL("9999", "失败"),
+    /**异常*/
+    ERROR("0000", "未知错误,请联系管理员"),
+    /**校验异常*/
+    REVIEW("0001", "校验异常");
+
+    /**回复码*/
+    private String code;
+    /**回复信息*/
+    private String msg;
+
+    BaseCode(String code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    /**
+     * <p>description : 用回复码返回对应枚举类型</p>
+     *
+     * @author jerryniu
+     * @version 1.0.0
+     */
+    public static BaseCode getBaseCodeByCode(String code) {
+        for (BaseCode baseCode : BaseCode.values()) {
+            if (baseCode.code.equals(code)) {
+                return baseCode;
+            }
+        }
+        return null;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
---
+++ samples/springboot-multi/springboot-multi-web/src/main/java/com/geercode/creed/samples/web/common/BaseResp.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2018-2050 the original author or authors.
+ * Powered by Yimei Corp.
+ * All Rights Reserved.
+ */
+
+package com.geercode.creed.samples.web.common;
+
+import lombok.Data;
+
+/**
+ * <p>description : 基础回复类</p>
+ *
+ * @author jerryniu
+ * @version 1.0.0
+ */
+@Data
+public class BaseResp<T> {
+    /**描述*/
+    private String msg;
+    /**编码*/
+    private String code;
+    /**数据内容*/
+    private T data;
+
+    /**
+     * <p>description : 默认成功(不返回数据)</p>
+     *
+     * @author jerryniu
+     * @version 1.0.0
+     */
+    public static BaseResp success() {
+        return BaseResp.discuss(BaseCode.SUCCESS, null);
+    }
+
+    /**
+     * <p>description : 成功(返回数据)</p>
+     *
+     * @author jerryniu
+     * @version 1.0.0
+     */
+    public static <T> BaseResp<T> success(T data) {
+        return BaseResp.discuss(BaseCode.SUCCESS, data);
+    }
+
+    /**
+     * <p>description : 默认失败(不返回数据)</p>
+     *
+     * @author jerryniu
+     * @version 1.0.0
+     */
+    public static BaseResp fail() {
+        return BaseResp.discuss(BaseCode.FAIL, null);
+    }
+
+    /**
+     * <p>description : 失败(返回描述)</p>
+     *
+     * @author jerryniu
+     * @version 1.0.0
+     */
+    public static BaseResp fail(String msg) {
+        return BaseResp.custom(BaseCode.FAIL.getCode(), BaseCode.FAIL.getMsg() + ":" + msg, null);
+    }
+
+    /**
+     * <p>description : 失败(返回数据)</p>
+     *
+     * @author jerryniu
+     * @version 1.0.0
+     */
+    public static <T> BaseResp<T> fail(T data) {
+        return BaseResp.discuss(BaseCode.FAIL, data);
+    }
+
+    /**
+     * <p>description : 提示校验异常</p>
+     *
+     * @author jerryniu
+     * @version 1.0.0
+     */
+    public static BaseResp review() {
+        return BaseResp.discuss(BaseCode.REVIEW, null);
+    }
+
+    /**
+     * <p>description : 提示校验异常(返回信息)</p>
+     *
+     * @author jerryniu
+     * @version 1.0.0
+     */
+    public static BaseResp review(String msg) {
+        return BaseResp.custom(BaseCode.REVIEW.getCode(), BaseCode.REVIEW.getMsg() + ":" + msg, null);
+    }
+
+    /**
+     * <p>description : 提示校验异常(返回数据)</p>
+     *
+     * @author jerryniu
+     * @version 1.0.0
+     */
+    public static <T> BaseResp<T> review(T data) {
+        return BaseResp.discuss(BaseCode.REVIEW, data);
+    }
+
+    /**
+     * <p>description : 错误</p>
+     *
+     * @author jerryniu
+     * @version 1.0.0
+     */
+    public static BaseResp error() {
+        return BaseResp.discuss(BaseCode.ERROR, null);
+    }
+
+    /**
+     * <p>description : 自定义返回码与数据(需要在BaseCode中建立新的枚举类)</p>
+     *
+     * @author jerryniu
+     * @version 1.0.0
+     */
+    public static <T> BaseResp<T> custom(BaseCode code, T data) {
+        BaseResp<T> resp = new BaseResp();
+        resp.setCode(code.getCode());
+        resp.setMsg(code.getMsg());
+        resp.setData(data);
+        return resp;
+    }
+
+    /**
+     * <p>description : 自定义返回码与数据</p>
+     *
+     * @author jerryniu
+     * @version 1.0.0
+     */
+    public static <T> BaseResp<T> custom(String code, String msg, T data) {
+        BaseResp<T> resp = new BaseResp();
+        resp.setCode(code);
+        resp.setMsg(msg);
+        resp.setData(data);
+        return resp;
+    }
+
+    private static <T> BaseResp<T> discuss(BaseCode code, T data) {
+        BaseResp<T> resp = new BaseResp();
+        resp.setCode(code.getCode());
+        resp.setMsg(code.getMsg());
+        resp.setData(data);
+        return resp;
+    }
+}
