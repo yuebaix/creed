6bc01b39983f0dba5045fdaa3266b2d23f6103a7
geercode <huifumanlove@hotmail.com>
2019-02-13 15:44:46 +0800

    解决多版本依赖问题

--- samples/sc-consul-app/src/main/java/com/geercode/creed/config/MdcFilter.java
+++ samples/sc-consul-app/src/main/java/com/geercode/creed/config/MdcFilter.java
@@ -1,7 +1,17 @@
 /*
  * Copyright 2018-2050 the original author or authors.
- * Powered by Yimei Corp.
- * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.geercode.creed.config;
@@ -52,7 +62,7 @@ public class MdcFilter implements Filter {
     public void destroy() {
     }
 
-    void insertIntoMDC(ServletRequest request) {
+    private void insertIntoMDC(ServletRequest request) {
         //对应logstash-gelf,添加请求发起时间
         MDC.put("profiling.requestStart.millis", "" + System.currentTimeMillis());
         //添加请求详细
@@ -72,7 +82,7 @@ public class MdcFilter implements Filter {
         }
     }
 
-    void clearMDC() {
+    private void clearMDC() {
         //对应logstash-gelf,清除请求发起时间
         MDC.remove("profiling.requestStart.millis");
         //清除请求详细
