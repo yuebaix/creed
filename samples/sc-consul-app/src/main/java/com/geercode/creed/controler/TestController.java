feb3ac1d0f6d59e93e82d03414267f81f0dbefff
geercode <huifumanlove@hotmail.com>
2019-02-21 20:12:04 +0800

    调试完成

--- samples/sc-consul-app/src/main/java/com/geercode/creed/controler/TestController.java
+++ samples/sc-consul-app/src/main/java/com/geercode/creed/controler/TestController.java
@@ -24,6 +24,7 @@ import com.geercode.creed.facade.service.ConsulServiceFeignService;
 import com.geercode.creed.stub.DemoDubboService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementServerProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
