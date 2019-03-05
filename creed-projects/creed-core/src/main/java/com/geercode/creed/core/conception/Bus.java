b69c3b0254a20674d8f3faa9b19d442cd9269e91
niupengfei <niupengfei@juxiangfen.com>
2019-03-05 17:34:22 +0800

    对应文档进行一些修改

---
+++ creed-projects/creed-core/src/main/java/com/geercode/creed/core/conception/Bus.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2018-2050 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.geercode.creed.core.conception;
+
+/**
+ * <p>Description : 总线</p>
+ * <p>Created on  : 2019-02-26 15:18</p>
+ *
+ * @author jerryniu
+ * @since 1.0.0
+ */
+public interface Bus {
+}
